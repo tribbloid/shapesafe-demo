976b054925bc94568d497448bf3b1e5abe731aee
tribbloid <pc175@uow.edu.au>
2021-03-09 20:13:19 -0500

    checkEinSum now supports ->* operator
    completed 2 showcases

---
+++ src/main/scala/org/shapesafe/demo/core/S1_NDTensor.scala
@@ -0,0 +1,26 @@
+package org.shapesafe.demo.core
+
+import org.shapesafe.core.arity.ops.ArityOps
+import org.shapesafe.core.shape.Shape
+
+object S1_NDTensor extends App {
+
+//  val s1 = Shape(3).named("i")
+  val s1 = Shape(2).named("i")
+  val s2 = s1 >< s1
+  val s3 = s2 >< s2
+  val s4 = s3 >< s3
+
+  println(s4)
+
+  val s4E = s4.eval
+  println(s4E)
+
+  val flat = s4.flatten(ArityOps.:*)
+  val flatE = flat.eval
+  println(flatE)
+
+  val w = flat dot Shape(256) // shortcut for ("i") einSum ("i") ->* ("i")
+  val wE = w.eval
+  println(wE)
+}
---
+++ src/main/scala/org/shapesafe/demo/core/S2_EinSum.scala
@@ -0,0 +1,21 @@
+package org.shapesafe.demo.core
+
+import org.shapesafe.core.arity.ops.ArityOps
+import org.shapesafe.core.shape.Shape
+
+object S2_EinSum extends App {
+
+  val s1 = Shape(999999, 8, 3)
+
+  val s2 = Shape(8, 361, 3)
+
+  val m = s1
+    .named("i", "j", "channel")
+    .einSum(
+      s2.named("j", "k", "channel")
+    ) ->* ("i", "k", "channel")
+  println(m)
+
+  val mE = m.eval
+  println(mE)
+}
