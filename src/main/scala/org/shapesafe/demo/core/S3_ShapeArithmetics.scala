96377a2ce40fb047120173702fdd27601efe22cb
tribbloid <pc175@uow.edu.au>
2021-03-23 18:00:57 -0400

    simplify type hierarchy of Arity & Axis
    LeafOps.>|< operator can now be used on non-parametric Axis using only dependent type

--- src/main/scala/org/shapesafe/demo/core/S3_ShapeArithmetics.scala
+++ src/main/scala/org/shapesafe/demo/core/S3_ShapeArithmetics.scala
@@ -1,5 +1,6 @@
 package org.shapesafe.demo.core
 
+import org.shapesafe.core.arity.ArityAPI
 import org.shapesafe.core.arity.ops.ArityOps
 import org.shapesafe.core.shape.{Names, Shape}
 
@@ -9,18 +10,16 @@ object S3_ShapeArithmetics extends App {
 
   def conv2D[
       S1 <: Shape,
-      S2 <: Shape,
-      PAD <: Shape.Vector,
-      STRD <: Shape.Vector
+      S2 <: Shape
   ](
       signal: S1,
       kernel: S2,
-      padding: PAD,
-      stride: STRD
+      padding: ArityAPI,
+      stride: ArityAPI
   ) = {
 
-    val _padding = (padding >< padding).withNames(ij)
-    val _stride = (stride >< stride).withNames(ij)
+    val _padding = (Shape >|< padding >|< padding).withNames(ij)
+    val _stride = (Shape >|< stride >|< stride).withNames(ij)
 
     signal
       .withNames(ij)
@@ -33,7 +32,7 @@ object S3_ShapeArithmetics extends App {
   val kernel = Shape(3, 3)
 
 //  val s2 = conv2D(s1, kernel, Arity.fromSInt(1), Arity.fromSInt(2))
-  val s2 = conv2D(s1, kernel, Shape(1), Shape(2))
+  val s2 = conv2D(s1, kernel, 1, 2)
   println(s2)
 
   val s2E = s2.eval
