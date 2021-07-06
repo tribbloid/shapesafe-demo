a5fba676bdec5b9dfc070993fec72c48aae68a7e
peng <pc175@uow.edu.au>
2021-07-05 20:07:50 -0400

    move an example into c2

---
+++ src/main/scala/org/shapesafe/demo/core/c2/S1.scala
@@ -0,0 +1,20 @@
+package org.shapesafe.demo.core.c2
+
+import org.shapesafe.core.shape.Shape
+
+object S1 extends App {
+
+  val v1 = Shape(4)
+  val v2 = Shape(3)
+
+  v1.dot(v1).reason
+  // v1.dot(v2).reason
+  v2.cross(v2).reason
+  v1.cross(v1).reason // FIXME: this is wrong!
+
+  val m1 = Shape(3, 4)
+  val m2 = Shape(4, 5)
+
+  m1.matMul(m2).reason
+  // m2.matMul(m1).reason
+}
