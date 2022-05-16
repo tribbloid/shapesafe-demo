1cbaf6df12a60f87c8df8dca15c41c95cfa9ab25
Peng Cheng <pc175@uow.edu.au>
2022-05-16 01:22:42 -0400

    first draft, use TryCompile in splain plugin for assertion

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,5 @@ dependencies {
 
     implementation(project(":core"))
     testImplementation(testFixtures(project(":core")))
-    testImplementation("io.tryp:splain_${vs.scalaV}:1.0.1")
     //Caution: NOT the same version as compiler plugin. Should always depend on a published release
 }
